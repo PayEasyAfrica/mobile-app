ef1898e483701446fb745d711f321b54e5f14542
Ojonugwa Alikali <alikali.ojonugwajustice@gmail.com>
2023-03-05 15:39:45 +0100

    Implement Notification Screen

---
+++ screens/Settings/ NotificationsScreen/NotificationsScreen.styles.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+import { FONT_400, FONT_500 } from '../../../constants/Style';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingHorizontal: 24
+	},
+	sectionContainer: { paddingTop: 32, paddingBottom: 14 },
+	sectionName: {
+		fontSize: 14,
+		fontFamily: FONT_500,
+		color: '#B4A9A5'
+	},
+	NotificationsItem: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingVertical: 16,
+		borderTopWidth: StyleSheet.hairlineWidth
+	},
+	NotificationslastItem: {
+		borderBottomWidth: StyleSheet.hairlineWidth
+	},
+
+	NotificationsItemText: {
+		fontSize: 16,
+		fontFamily: FONT_400,
+		marginHorizontal: 14
+	}
+});
+
+export default styles;
---
+++ screens/Settings/ NotificationsScreen/NotificationsScreen.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { SectionList, Switch, View as RNView } from 'react-native';
+import { MailIcon, PersonIcon } from '../../../components/CustomIcons';
+
+import { SafeAreaView, Text, View } from '../../../components/Themed';
+import Colors from '../../../constants/Colors';
+import useColorScheme from '../../../hooks/useColorScheme';
+import styles from './NotificationsScreen.styles';
+
+interface NotificationsItem {
+	name: string;
+	slug: string;
+	icon: (color: string) => JSX.Element;
+}
+
+interface NotificationStates {
+	[key: string]: boolean;
+}
+
+const DATA = [
+	{
+		sectionName: 'Login Alerts',
+		data: [
+			{
+				name: 'SMS',
+				slug: 'login_sms',
+				icon: (color: string) => <PersonIcon color={color} />
+			}
+		] as NotificationsItem[]
+	},
+	{
+		sectionName: 'Transaction Alerts',
+		data: [
+			{
+				name: 'Email',
+				slug: 'transaction_email',
+				icon: (color: string) => <MailIcon color={color} />
+			},
+			{
+				name: 'SMS',
+				slug: 'transaction_sms',
+				icon: (color: string) => <PersonIcon color={color} />
+			}
+		] as NotificationsItem[]
+	}
+];
+
+export default function NotificationsScreen() {
+	const [notificationStates, setNotificationStates] =
+		useState<NotificationStates>({});
+
+	const colorScheme = useColorScheme();
+	const { border, text: textColor } = Colors[colorScheme];
+
+	const toggleSwitch = (name: string) => {
+		setNotificationStates((prevState) => ({
+			...prevState,
+			[name]: !prevState[name]
+		}));
+	};
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<SectionList
+				sections={DATA}
+				keyExtractor={(item, index) => item.name + index}
+				renderItem={({ item, index, section }) => (
+					<View
+						style={[
+							styles.NotificationsItem,
+							index === section.data.length - 1 && styles.NotificationslastItem,
+							{
+								borderColor: border
+							}
+						]}
+					>
+						{item.icon(textColor)}
+						<Text style={[styles.NotificationsItemText]}>{item.name}</Text>
+
+						<RNView style={{ marginLeft: 'auto' }}>
+							<Switch
+								trackColor={{ false: '#D0C4C0', true: '#EF672A' }}
+								thumbColor={
+									notificationStates[item.slug] ? '#FFFFFF' : '#FBEEEA'
+								}
+								ios_backgroundColor="#D0C4C0"
+								onValueChange={() => toggleSwitch(item.slug)}
+								value={notificationStates[item.slug]}
+							/>
+						</RNView>
+					</View>
+				)}
+				renderSectionHeader={({ section: { sectionName } }) => (
+					<View style={styles.sectionContainer}>
+						<Text style={styles.sectionName}>{sectionName}</Text>
+					</View>
+				)}
+			/>
+		</SafeAreaView>
+	);
+}
